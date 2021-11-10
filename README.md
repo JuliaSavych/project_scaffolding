ba1fb72854f6ed2600f71dee60aeacedcc564ac6
Julia <yuliia.savych@vakoms.com>
2021-11-10 12:03:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Dependencies
+
+Use the package manager [npm](https://www.npmjs.com/) to install all dependencies.
+
+```
+npm install
+```
+## Build project and start live server
+```
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const prefix = require('gulp-autoprefixer');
+
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: './app/'
+        },
+        notify: false
+    });
+}
+
+function watchFiles() {
+    watch("app/scss/**/*.scss", css);
+    watch("app/*.html").on('change', browserSync.reload);
+    watch('app/assets/**/*').on('change', browserSync.reload);
+}
+
+function css() {
+    return src("app/scss/**/*.scss")
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+        .pipe(dest("app"))
+        .pipe(browserSync.stream());
+}
+
+exports.css = css;
+exports.default = series(series(css), parallel(browser, watchFiles));
